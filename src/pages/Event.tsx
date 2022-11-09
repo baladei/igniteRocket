cd2a58460a34a1dcc5fb542cf2fb022b407ddae8
Fernando Baladei <f.baladei@gmail.com>
2022-11-09 04:59:26 -0300

    updating issues third class

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,13 @@
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
-import { testando } from "../components/Player";
+import { Video } from "../components/testando";
 
 export function Event () {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex flex-1">
-                <Player />
+                <Video />
                 <Sidebar />
             </main>
         </div>
